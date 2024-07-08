42c34746e732fc4054ea80bee38945815eceb1b7
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-07-07 21:06:47 -0400

    Add timed transition & link navdots to corresponding slide

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,7 @@ export function transition() {
   let imgIndex;
   // NEXT IMG BUTTON
   next.addEventListener("click", () => {
+    let navDot;
     let nextImgIndex;
     images.forEach((img) => {
       console.log(images);
@@ -15,6 +16,8 @@ export function transition() {
         const currentImg = img;
         currentImg.style.display = "none";
         imgIndex = parseInt(currentImg.getAttribute("data-index-number"));
+        navDot = document.getElementById(`dot${imgIndex}`);
+        navDot.style.backgroundColor = "whitesmoke";
         if (imgIndex == images.length) {
           nextImgIndex = 1;
         } else if (imgIndex < images.length) {
@@ -23,12 +26,15 @@ export function transition() {
       }
     });
     const nextImg = document.getElementById(`img${nextImgIndex}`);
+    navDot = document.getElementById(`dot${nextImgIndex}`);
+    navDot.style.backgroundColor = "red";
     nextImg.style.display = "block";
   });
 
   // PREVIOUS IMG BUTTON
   previous.addEventListener("click", () => {
     let prevImgIndex;
+    let navDot;
     images.forEach((img) => {
       // get & hide current img & display previous img
       const styles = window.getComputedStyle(img);
@@ -36,6 +42,8 @@ export function transition() {
         const currentImg = img;
         currentImg.style.display = "none";
         imgIndex = parseInt(currentImg.getAttribute("data-index-number"));
+        navDot = document.getElementById(`dot${imgIndex}`);
+        navDot.style.backgroundColor = "whitesmoke";
         if (imgIndex == 1) {
           prevImgIndex = images.length;
         } else if (imgIndex > 1) {
@@ -44,6 +52,33 @@ export function transition() {
       }
     });
     const prevImg = document.getElementById(`img${prevImgIndex}`);
+    navDot = document.getElementById(`dot${prevImgIndex}`);
+    navDot.style.backgroundColor = "red";
     prevImg.style.display = "block";
+  });
+}
+
+function timedTransition() {
+  next.click();
+}
+
+export function transitionControl() {
+  let intervalId;
+  document.addEventListener("DOMContentLoaded", () => {
+    intervalId = setInterval(timedTransition, 5000);
+  });
+  next.addEventListener("mouseover", () => {
+    clearInterval(intervalId);
+  });
+  next.addEventListener("mouseleave", () => {
+    intervalId = setInterval(timedTransition, 5000);
+  });
+
+  previous.addEventListener("mouseover", () => {
+    clearInterval(intervalId);
+  });
+
+  previous.addEventListener("mouseleave", () => {
+    intervalId = setInterval(timedTransition, 5000);
   });
 }
